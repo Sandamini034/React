7a7709ea8da966ee34d0aef0f7a3b19fc7e09bb7
Honali Sandamini <hsandaminih@gmail.com>
2026-04-28 10:25:08 +0530

    modified the folder

--- my-react-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Signup from "./signup.jsx"
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Signup/>
-  </StrictMode>
-)
